471de5b5dc2be08d627f687adddccb607554e95c
litao <litao163email@163.com>
2023-03-11 00:30:04 +0800

    add 引入Echarts并对API接口进行统计

--- config/routes.ts
+++ config/routes.ts
@@ -7,17 +7,18 @@ export default [
   },
   { name: 'API列表', icon: 'table', path: '/list', component: './Index' },
   { name: 'API详情', icon: 'table', path: '/list/details/:id', component: './InterfaceDetails', hideInMenu:true},
-  {
-    name: '管理页',
-    path: '/admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      { path: '/admin', redirect: '/admin/sub-page' },
-      { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
-    ],
-  },
-  { name: '管理API', icon: 'table', access: 'canAdmin',path: '/list/manger', component: './InterfaceInfo' },
-  { path: '/', redirect: '/list/manger' },
+  { path: '/list/manger', icon: 'table',name: 'API管理', component: './Admin/InterfaceInfo' },
+  { path: '/list/analysis', icon: 'table',name: 'API分析', component: './Admin/InterfaceAnalysis' },
+  // {
+  //   name: 'API管理系统',
+  //   path: '/admin',
+  //   icon: 'crown',
+  //   // access: 'canAdmin',
+  //   routes: [
+  //     { path: '/list/manger', name: 'API管理', component: './Admin/InterfaceInfo' },
+  //     { path: '/list/analysis', name: 'API分析', component: './Admin/InterfaceAnalysis' },
+  //   ],
+  // },
+  { path: '/', redirect: '/list' },
   { path: '*', layout: false, component: './404' },
 ];
---
+++ src/pages/Admin/InterfaceAnalysis/index.tsx
@@ -0,0 +1,90 @@
+import {} from 'antd/es/table/interface';
+import React, {useEffect, useState} from 'react';
+import ReactECharts from 'echarts-for-react';
+import { message} from 'antd';
+import {listTopInvokeInterfaceInfoUsingGET} from '@/services/fastApi/analysisController';
+
+
+/**
+ * 页面主入口
+ * @constructor
+ */
+const TableList: React.FC = () => {
+  const [data, setData] = useState<API.InterfaceInfoVO[]>([]);
+  /**
+   * @en-US Update node
+   * @zh-CN API分析饼状图
+   * @param fields
+   */
+  const loadData = async () => {
+    const hide = message.loading('加载中');
+    try {
+      const res = await listTopInvokeInterfaceInfoUsingGET({});
+      if (res.data) {
+        setData(res.data ?? []);
+        message.success('加载成功');
+        hide();
+        return true;
+      }
+    } catch (error) {
+      hide();
+      message.error('加载失败');
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  // 映射：{ value: 1048, name: 'Search Engine' },
+  const chartData = data.map(item => {
+    return {
+      value: item.totalNum,
+      name: item.name,
+    }
+  })
+
+  /**
+   *
+   * tooltip 显示当前扇形信息
+   */
+  const option = {
+    title : {
+      text: 'API调用次数',
+      subtext: '截止到当前时间为止',
+      x:'center'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    series: [
+      {
+        name: 'API调用次数',
+        type: 'pie',
+        radius: [50, 250],
+        center: ['50%', '50%'],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 8,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        },
+        data: chartData
+      }
+    ]
+  };
+
+  return (
+    <ReactECharts option={option} style={{height: 600}}/>
+  )
+    ;
+};
+
+export default TableList;
